2023ec4f8b587a412be2a0ce7b5a55d937c5e539
Nikko Elliott <53981746+nelliott82@users.noreply.github.com>
2023-03-24 16:04:15 -0300

    Update airport.js with more runways

--- airport.js
+++ airport.js
@@ -14,11 +14,30 @@ I created a few variables and logic to halt a plane's current landing attempt an
 - The current landing plane has enough fuel (at least 81 liters + its current altitude) to wait for another plane to make its full landing attempt before starting to land again.
 - The other plane being considered for reprioritization has less fuel than the current landing plane AND has enough fuel to make a safe landing.
 
-I also built in a loop to run the simulation 1,000 times for up to 10,000 frames each time. Half of the simulations reprioritize landing planes (test group) and half of
+I also built in a loop to run the simulation n times for up to 10,000 frames each time. Half of the simulations reprioritize landing planes (test group) and half of
 them do not (control group). The total number of crashes each run are recorded in an object (crashTally) to compare the rates of crashes.
 
 Future work to consider:
 - Adding n more runways.
+
+************** Adding n more runways ****************
+
+After a few experiments, it was shown, unsurprisingly, that adding more runways reduced crashes.
+
+How more runways works:
+
+- A new variable is added to each simulation's tick() called "runways" equal to a chosen integer
+- A second new variable called landingPlanes set as an empty array is also created each run of the simulation's tick()
+- For each airplane that is landing, the "runways" variable is reduced by one and the airplane is added to the landingPlanes array
+- If there are still runways available after checking all airplanes, the lowest fueled airplane not landing is set to land
+- If there are no runways available, the same reprioritizing logic as before is applied, except it now checks the landingPlanes array for the
+    mostFueledPlane and halts its landing
+
+Surprises:
+- It was assumed going in that a second runway would be sufficient to reduce crashes to zero, but it takes as many as six runways
+    to have zero crashes consistently (although many many interations may prove more runways are needed)
+
+*****************************************************
 */
 
 // returns a Promise which resolves after `ms` milliseconds
@@ -26,7 +45,17 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let crashTally = { controls: 0, tests: 0 }
+function log(frameNumber, message) {
+    console.log(
+        `\x1b[2m[Frame #${frameNumber.toString().padStart(4, '0')}] \x1b[0m` +
+        message
+    );
+}
+
+/* THE FOLLOWING ARE FOR TESTING PURPOSES */
+
+let maxRuns = 10000;
+let crashTally = { controls: 0, tests: 0 };
 
 function recordCrashes (control, airplanes) {
     let crashed = 0;
@@ -44,12 +73,8 @@ function recordCrashes (control, airplanes) {
     }
 }
 
-function log(frameNumber, message) {
-    console.log(
-        `\x1b[2m[Frame #${frameNumber.toString().padStart(4, '0')}] \x1b[0m` +
-        message
-    );
-}
+/* END OF TESTING UTILITIES */
+
 
 class Airplane {
     constructor(planeNumber) {
@@ -67,14 +92,14 @@ class Airplane {
             this.fuel -= this.fuelRate;
 
             if (this.landing) {
-                this.handleLanding();
+                this.handleLanding(frameNumber);
             } else {
-                this.handleCrash();
+                this.handleCrash(frameNumber);
             }
         }
     }
 
-    handleLanding() {
+    handleLanding(frameNumber) {
         this.altitude -= this.fuelRate;
         if (!this.altitude) {
             this.landing = false;
@@ -83,7 +108,7 @@ class Airplane {
         }
     }
 
-    handleCrash() {
+    handleCrash(frameNumber) {
         if (this.fuel <= 0) {
             this.crashed = true;
             //log(frameNumber, `☠️  Plane ${this.planeNumber} crashed`);
@@ -108,15 +133,16 @@ class Simulation {
 
         recordCrashes(this.control, this.airplanes);
 
-        if (this.runNumber === 100) {
+        if (this.runNumber === maxRuns) {
             console.log(crashTally)
         }
     }
 
     tick() {
         this.frameNumber += 1;
+        let runways = 6;
         let planeLanding = false;
-        let landingPlane = {};
+        let landingPlanes = [];
         let lowestAirplane = { fuel: Infinity };
 
         this.generateAirplane();
@@ -124,22 +150,28 @@ class Simulation {
         this.airplanes.forEach((airplane) => {
             if (!airplane.landed) {
                 airplane.tick(this.frameNumber);
-            }
-            if (airplane.landing) {
-                landingPlane = airplane;
-                planeLanding = true;
-            }
-            if (airplane.fuel < lowestAirplane.fuel &&
-                    airplane.fuel >= 80 &&
-                    !airplane.landed) {
-                lowestAirplane = airplane;
+
+                if (airplane.landing) {
+                    if (!this.control) {
+                        runways -= 1;
+                    }
+                    landingPlanes.push(airplane);
+                    planeLanding = true;
+                } else if (airplane.fuel < lowestAirplane.fuel &&
+                            airplane.fuel >= airplane.altitude) {
+                    lowestAirplane = airplane;
+                }
             }
         });
 
-        this.startPlaneLanding(planeLanding, lowestAirplane);
-
-        if (landingPlane.planeNumber && !this.control) {
-            this.prioritizeLandingOtherPlane(lowestAirplane, landingPlane);
+        if (this.control) {
+            !planeLanding && this.startPlaneLanding(lowestAirplane);
+        } else {
+            if (runways) {
+                this.startPlaneLanding(lowestAirplane);
+            } else {
+                this.prioritizeLandingOtherPlane(lowestAirplane, landingPlanes);
+            }
         }
     }
 
@@ -156,9 +188,8 @@ class Simulation {
         }
     }
 
-    startPlaneLanding(planeLanding, lowestAirplane) {
-        if (!planeLanding &&
-                this.airplanes.length &&
+    startPlaneLanding(lowestAirplane) {
+        if (this.airplanes.length &&
                 lowestAirplane.planeNumber) {
             lowestAirplane.landing = true;
 
@@ -166,19 +197,25 @@ class Simulation {
         }
     }
 
-    prioritizeLandingOtherPlane(lowestAirplane, landingPlane) {
-        if (lowestAirplane.fuel < landingPlane.fuel &&
-                landingPlane.fuel >= (81 + landingPlane.altitude)) {
-            landingPlane.landing = false;
-            //log(this.frameNumber, `Plane ${landingPlane.planeNumber} landing has been halted.`);
+    prioritizeLandingOtherPlane(lowestAirplane, landingPlanes) {
+        let mostFueledPlane = { fuel: -Infinity };
+
+        for (let i = 0; i < landingPlanes.length; i++) {
+            if (mostFueledPlane.fuel > landingPlanes[i].fuel) {
+                mostFueledPlane = landingPlanes[i];
+            };
+        }
+
+        if (lowestAirplane.fuel < mostFueledPlane.fuel &&
+                mostFueledPlane.fuel >= (81 + mostFueledPlane.altitude)) {
+            mostFueledPlane.landing = false;
             lowestAirplane.landing = true;
-            //log(this.frameNumber, `Plane ${lowestAirplane.planeNumber} is landing.`);
         }
     }
 }
 
 console.log("running Simulation");
-for (let i = 1; i <= 1000; i++) {
+for (let i = 1; i <= maxRuns; i++) {
     let s = new Simulation(i, i % 2 === 0);
     s.run();
 }
